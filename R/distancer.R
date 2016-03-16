3f9198805ff308cb52ae8ab315bf45d8672547c6
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-16 16:16:53 +0100

    EOD

---
+++ R/distancer.R
@@ -0,0 +1,8 @@
+#' Do the simulation
+#' @param parameters the simulation parameters
+#' @return a data table
+#' @useDynLib distancer
+#' @importFrom Rcpp sourceCpp
+do_simulation_r <- function(parameters) {
+  parameters
+}
