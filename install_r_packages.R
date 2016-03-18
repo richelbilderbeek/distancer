c5d257a1d4d4a0adbfada23af07da9f5ea9093a5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-18 09:22:02 +0100

    Travis: install R packages

---
+++ install_r_packages.R
@@ -0,0 +1,5 @@
+install.packages("Rcpp", repos = "http://cran.uk.r-project.org")
+install.packages("knitr", repos = "http://cran.uk.r-project.org")
+install.packages("rmarkdown", repos = "http://cran.uk.r-project.org")
+install.packages("testthat", repos = "http://cran.uk.r-project.org")
+install.packages("ggplot2", repos = "http://cran.uk.r-project.org")
