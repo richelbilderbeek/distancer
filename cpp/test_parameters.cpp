7949dbf8c6b962db3a59528392f676d341a540aa
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-27 15:22:02 +0200

    Fixed unit tests

--- cpp/test_parameters.cpp
+++ cpp/test_parameters.cpp
@@ -11,138 +11,150 @@
 BOOST_AUTO_TEST_CASE(test_parameters)
 {
   const int max_genetic_distance{1};
-  const double mutation_rate{0.1};
   const int n_generations{10};
   const std::size_t n_pin_loci{2};
   const std::size_t n_sil_loci{2};
+  const double pin_mutation_rate{0.1};
   const int population_size{10};
   const std::string results_filename{"tmp.txt"};
   const int rng_seed{42};
   const int sampling_interval{1};
+  const double sil_mutation_rate{0.1};
 
   BOOST_CHECK_NO_THROW(
     parameters(
       max_genetic_distance,
-      mutation_rate,
       n_generations,
       n_pin_loci,
       n_sil_loci,
+      pin_mutation_rate,
       population_size,
       results_filename,
       rng_seed,
-      sampling_interval
+      sampling_interval,
+      sil_mutation_rate
     )
   );
 
   BOOST_CHECK_THROW(
     parameters(
       -1, //max_genetic_distance,
-      mutation_rate,
       n_generations,
       n_pin_loci,
       n_sil_loci,
+      pin_mutation_rate,
       population_size,
       results_filename,
       rng_seed,
-      sampling_interval
+      sampling_interval,
+      sil_mutation_rate
     ),
     std::invalid_argument
   );
 
+
   BOOST_CHECK_THROW(
     parameters(
       max_genetic_distance,
-      -0.1, //mutation_rate,
-      n_generations,
-      n_pin_loci,
-      n_sil_loci,
-      population_size,
-      results_filename,
-      rng_seed,
-      sampling_interval
-    ),
-    std::invalid_argument
-  );
-  BOOST_CHECK_THROW(
-    parameters(
-      max_genetic_distance,
-      1.1, //mutation_rate,
-      n_generations,
-      n_pin_loci,
-      n_sil_loci,
-      population_size,
-      results_filename,
-      rng_seed,
-      sampling_interval
-    ),
-    std::invalid_argument
-  );
-  BOOST_CHECK_THROW(
-    parameters(
-      max_genetic_distance,
-      mutation_rate,
       -1, //n_generations,
       n_pin_loci,
       n_sil_loci,
+      pin_mutation_rate,
       population_size,
       results_filename,
       rng_seed,
-      sampling_interval
+      sampling_interval,
+      sil_mutation_rate
     ),
     std::invalid_argument
   );
   BOOST_CHECK_NO_THROW(
     parameters(
       max_genetic_distance,
-      mutation_rate,
       n_generations,
       -1, //n_loci, gets implicitly converted to unsigned integer
       -1, //same here
+      pin_mutation_rate,
       population_size,
       results_filename,
       rng_seed,
-      sampling_interval
+      sampling_interval,
+      sil_mutation_rate
     )
+  );
+
+  BOOST_CHECK_THROW(
+    parameters(
+      max_genetic_distance,
+      n_generations,
+      n_pin_loci,
+      n_sil_loci,
+      -0.1, //pin_mutation_rate,
+      population_size,
+      results_filename,
+      rng_seed,
+      sampling_interval,
+      sil_mutation_rate
+    ),
+    std::invalid_argument
   );
   BOOST_CHECK_THROW(
     parameters(
       max_genetic_distance,
-      mutation_rate,
       n_generations,
       n_pin_loci,
       n_sil_loci,
+      1.1, //pin_mutation_rate,
+      population_size,
+      results_filename,
+      rng_seed,
+      sampling_interval,
+      sil_mutation_rate
+    ),
+    std::invalid_argument
+  );
+  BOOST_CHECK_THROW(
+    parameters(
+      max_genetic_distance,
+      n_generations,
+      n_pin_loci,
+      n_sil_loci,
+      pin_mutation_rate,
       -1, //population_size,
       results_filename,
       rng_seed,
-      sampling_interval
+      sampling_interval,
+      sil_mutation_rate
     ),
     std::invalid_argument
   );
   BOOST_CHECK_THROW(
     parameters(
       max_genetic_distance,
-      mutation_rate,
       n_generations,
       n_pin_loci,
       n_sil_loci,
+      pin_mutation_rate,
       population_size,
       "", //results_filename,
       rng_seed,
-      sampling_interval
+      sampling_interval,
+      sil_mutation_rate
     ),
     std::invalid_argument
   );
   BOOST_CHECK_THROW(
     parameters(
       max_genetic_distance,
-      mutation_rate,
       n_generations,
       n_pin_loci,
       n_sil_loci,
+      pin_mutation_rate,
       population_size,
       results_filename,
       rng_seed,
-      -1 //sampling_interval
+      -1, //sampling_interval
+      sil_mutation_rate
     ),
     std::invalid_argument
   );
@@ -150,14 +162,15 @@ BOOST_AUTO_TEST_CASE(test_parameters)
   BOOST_CHECK_THROW(
     parameters(
       max_genetic_distance,
-      mutation_rate,
       10, //n_generations,
       n_pin_loci,
       n_sil_loci,
+      pin_mutation_rate,
       population_size,
       results_filename,
       rng_seed,
-      1000 //sampling_interval
+      1000, //sampling_interval
+      sil_mutation_rate
     ),
     std::invalid_argument
   );
